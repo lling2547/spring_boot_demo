6b85edc142a951aea9ea217c3e7d2eb0f6e46dfb
lling <lling2547@163.com>
2023-05-17 19:19:36 +0800

    提交spring_boot_demo下的SpringBoot_self

---
+++ src/main/java/com/zep/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.zep.controller;
+
+
+import com.zep.config.DataSourceProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableConfigurationProperties(DataSourceProperties.class)
+@RestController
+public class HelloController {
+
+    @Autowired
+    private DataSourceProperties dataSourceProperties;
+
+
+    @RequestMapping("/hello")
+    public String toHello(){
+        return "Hello Spring Boot Self";
+    }
+
+    @RequestMapping("/dataSource")
+    public String toDataSource(){
+        System.out.println(dataSourceProperties);
+        return "Hello SpringBoot DSP";
+    }
+
+}
